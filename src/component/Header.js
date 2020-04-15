c63501f86bc02ffbe44668bb31c746d34a53bf61
wahyu <wahyuritchie@gmail.com>
2020-04-15 21:23:28 +0700

    Fix Header a href to Link

--- src/component/Header.js
+++ src/component/Header.js
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom";
 export default Header => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a className="navbar-brand text-white" href="/"><b>Premier League Clubs</b></a>
+            <Link to={"/"} className="navbar-brand text-white"><b>Premier League Clubs</b></Link>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item">
